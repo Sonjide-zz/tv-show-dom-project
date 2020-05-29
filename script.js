312aff9c93e47a5ef01c014c2a378f40185cc937
Sonjide Hussain <sonjide.hussain@gmail.com>
2020-05-29 12:46:38 +0100

    added divs with correct column width, a scroll bar on the side

--- script.js
+++ script.js
@@ -16,39 +16,40 @@ Your page should state somewhere that the data has (originally) come from TVMaze
 */
 
 function setup() {
-    const allEpisodes = getAllEpisodes();
-    makePageForEpisodes(allEpisodes);
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
+  //this find the id that equal root which empty div in html
+  const rootElem = document.getElementById("root");
 
-    //this find the id that equal root which empty div in html
-    const rootElem = document.getElementById("root");
+  //attach a div with class of container to the child of the root element
+  const containerElm = document.createElement("div");
+  containerElm.classList.add("container");
+  rootElem.appendChild(containerElm);
+  //then attach another div with a class of "ROW" to the container element
+  const rowElm = document.createElement("div");
+  rowElm.classList.add("row");
+  containerElm.appendChild(rowElm);
 
-    //attach a div with class of container to the child of the root element
-    const containerElm = document.createElement("div");
-    containerElm.classList.add("container");
-    rootElem.appendChild(containerElm);
-    //then attach another div with a class of "ROW" to the container element
-    const rowElm = document.createElement("div");
-    rowElm.classList.add("row");
-    containerElm.appendChild(rowElm);
+  episodeList.forEach((episode) => {
+    //console.log(episode);
 
-    episodeList.forEach(episode => {
-        //console.log(episode);
+    //concatinate title - S01E01 - using interpolation
+    const epiSeasonNumber = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    console.log(epiSeasonNumber);
 
-        //concatinate title - S01E01 - using interpolation
-        const epiSeasonNumber = `${episode.name} - S0${episode.season}E0${episode.number }`;
-        console.log(epiSeasonNumber);
+    const image = episode.image.medium;
+    console.log(image);
 
-        const image = episode.image.medium;
-        console.log(image);
+    //episode summary - remove all the <p></p> tags inside the text using regex snippet
+    //source - https://css-tricks.com/snippets/javascript/strip-html-tags-in-javascript/
+    const summary = episode.summary.replace(/(<([^>]+)>)/gi, "");
+    console.log(summary);
 
-        //episode summary - remove all the <p></p> tags inside the text using regex snippet
-        //source - https://css-tricks.com/snippets/javascript/strip-html-tags-in-javascript/
-        const summary = episode.summary.replace(/(<([^>]+)>)/ig, "");
-        console.log(summary); 
-        const template = `<div class="episodeDiv col-4">
+    const template = `<div class="col-12 sm-col-6 md-col-4 lg-col-3">
+    <div class="episodeDiv">
                 <div class="episodeTitle">
                     <h5>${epiSeasonNumber}</h5>
                 </div>
@@ -58,19 +59,26 @@ function makePageForEpisodes(episodeList) {
                 <div class="summary">
                     <p>${summary}</p>
                 </div>
+            </div> 
             </div>`;
 
-        //then we are going to use the row element instead of the root element
-        rowElm.innerHTML = rowElm.innerHTML + template;
-        //console.log(rowElm.innerHTML);
-        //innerHTML -  https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/innerHTML
-
-    });
-};
-
+    //then we are going to use the row element instead of the root element
+    rowElm.innerHTML = rowElm.innerHTML + template;
+    //console.log(rowElm.innerHTML);
+    //innerHTML -  https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/innerHTML
+  });
+}
 
 window.onload = setup;
 
-// now fix styling for the div so they are 3 in a row
+//grid.css
+//grid.css important note - 4 level of div construction for your content
+// 1st div - outer container
+// 2nd div - flex-box row
+// 3rd div - column-width div - this is where you decide how many tv-shows you can see on the screen
+// div - for all the contents - this is where you put all the content of the tv-show
+// see example https://codepen.io/sonjide/pen/jObjMZg - if you remove each layer you will find that the content will be modified or not look as you intended
+
+// now fix styling for the div so they are 4 in a row - this is now solved
 // look at how to use.filter to filter episodes
 // outcome of second task is to only show the episodes which have the word“ game” in the name(or series)
